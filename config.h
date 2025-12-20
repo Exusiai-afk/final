784aff54539ed07b71c6e39cc5d34ba1acee04f9
Minghao <minghao.chen@student.kuleuven.be>
2025-12-20 11:19:01 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,7 +1,6 @@
 /**
- * \author {AUTHOR}
+ * \author {MINGHAO CHEN}
  */
-
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
@@ -10,12 +9,14 @@
 
 typedef uint16_t sensor_id_t;
 typedef double sensor_value_t;
-typedef time_t sensor_ts_t;         // UTC timestamp as returned by time() - notice that the size of time_t is different on 32/64 bit machine
+typedef time_t sensor_ts_t;
 
 typedef struct {
     sensor_id_t id;
     sensor_value_t value;
     sensor_ts_t ts;
 } sensor_data_t;
+
+int write_to_log_process(char *msg);
 
 #endif /* _CONFIG_H_ */
